6ef077dc153fe170e4c363c611c26f968db26eb8
Mark <mrak.mk@ya.ru>
2015-05-27 18:34:34 +1000

    Создание класса
    UserClass

--- test.php
+++ test.php
@@ -1,34 +1,46 @@
 <?php
-$way = $_POST["way"];
-$fp = file_get_contents("test/".$way.".txt");
-$arr = explode ("\n",$fp);
-
-$channelname = $arr[11];
-$channelname = substr($arr[11], 0, -1);
-$channels = explode(";", $channelname);
-
-$color = 0;
-
-for ($j=0; $j < count($channels); $j++,$color++) { 
+/**
+* 
+*/
+class UserClass
+{
 	
-	$z = 0;
-	
-	$result .= '{label:'.'\''.$channels[$j].'\''.','.'color:'.$color.','.'data:[';
-	
-	for ($n=12; $n < count($arr); $n++,$z++) { 
+	function dataFormat($way)
+	{
+		$fp = file_get_contents("test/".$way.".txt");
+		$arr = explode ("\n",$fp);
 
-		$str = explode(' ', $arr[$n]);
+		$channelname = $arr[11];
+		$channelname = substr($arr[11], 0, -1);
+		$channels = explode(";", $channelname);
 
-		$result .= '['.$z.','.$str[$j].'],';
+		$color = 0;
 
-	 	}
-	 $result .= ']},';
-	 
+		for ($j=0; $j < count($channels); $j++,$color++) { 
+			
+			$z = 0;
+			
+			$result .= '{label:'.'\''.$channels[$j].'\''.','.'color:'.$color.','.'data:[';
+			
+			for ($n=12; $n < count($arr); $n++,$z++) { 
+
+				$str = explode(' ', $arr[$n]);
+
+				$result .= '['.$z.','.$str[$j].'],';
+
+			 	}
+			 $result .= ']},';
+			 
+		}
+
+		$new = fopen('text.txt','w');
+		fwrite($new, $result);
+		fclose($new);
+		header('Location:index.php')
+	}
 }
-
-$new = fopen('text.txt','w');
-fwrite($new, $result);
-fclose($new);
-header('Location:index.php')
+	
+	$object = new UserClass;
+	$object->dataFormat($_POST['way']);
 ?>	
 
